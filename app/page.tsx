f53566a19fcef1fbf106d6f853424da4562460ac
Mahima Chacko <mahimamchacko@gmail.com>
2024-06-12 20:05:13 -0400

    Fix About page and add vertical padding to container

--- app/page.tsx
+++ app/page.tsx
@@ -3,50 +3,36 @@ import Badge from "@/components/badge";
 export default function Home() {
   const badges = [
     {
-      "id": 1,
-      "color": "bg-emerald-200",
-      "text": "software developer"
+      id: 1,
+      color: "bg-emerald-200",
+      text: "software developer"
     },
     {
-      "id": 2,
-      "color": "bg-teal-200",
-      "text": "software engineer"
+      id: 2,
+      color: "bg-teal-200",
+      text: "software engineer"
     },
     {
-      "id": 3,
-      "color": "bg-cyan-200",
-      "text": "software programmer"
+      id: 3,
+      color: "bg-cyan-200",
+      text: "software programmer"
     },
     {
-      "id": 4,
-      "color": "bg-sky-200",
-      "text": "student"
+      id: 4,
+      color: "bg-sky-200",
+      text: "student"
     },
   ];
 
   return (
-    <main className="container">
+    <main className="container flex flex-col gap-4 py-12">
       <h1>mahima chacko</h1>
-      <div className="flex flex-wrap gap-2 mt-3 mb-5">
+      <div className="flex flex-wrap gap-2">
         {badges.map(({ id, color, text }) => (
           <Badge key={id} color={color}>
             <h6>{text}</h6>
           </Badge>
         ))}
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div>
-          <p className="font-bold">who am i?</p>
-          <p>i'm a software engineering major with a minor in data science at drexel university. i'm passionate about creating innovate solutions to unique problems.</p>
-        </div>
-        <div>
-          <p className="font-bold">what's my experience?</p>
-          <p>i have work experience in full-stack development. i aspire to continue to improve my skills by gaining more experience in the industry.</p>
-        </div>
-        <div>
-          <p className="font-bold">what're my projects?</p>
-          <p>i've acted as the product owner in creating a web app with three other peers at drexel. outside of college, i aspire to learn new tools and technologies.</p>
-        </div>
       </div>
     </main>
   );
