dbddfe0ea9860d3c8c362e1c79ee9171f12735ef
Mahima Chacko <mahimamchacko@gmail.com>
2024-06-22 06:43:20 -0400

    Update Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,32 +6,35 @@ export default function Contact() {
   const tabs = [
     {
       id: 1,
-      title: "GMAIL",
+      title: "email",
       link: "mailto:mahimamchacko@gmail.com",
-      image: "/gmail_logo.png",
       color: "bg-red-200"
     },
     {
       id: 2,
-      title: "LINKEDIN",
+      title: "linkedin",
       link: "https://www.linkedin.com/in/mahimamchacko/",
-      image: "/linkedin_logo.png",
       color: "bg-sky-200"
+    },
+    {
+      id: 3,
+      title: "github",
+      link: "https://github.com/mahimamchacko/",
+      color: "bg-stone-200"
     }
   ]
 
   return (
     <main className="container flex flex-col gap-8 md:gap-12 py-12">
       <h1>contact</h1>
-      <div className="flex flex-col gap-6 md:gap-8">
-        {tabs.map(({ id, title, link, image, color }) => (
+      <div className="flex flex-wrap gap-2">
+        {tabs.map(({ id, title, link, color }) => (
           <div key={id} className="flex gap-8 md:gap-10">
-            <Link href={link} draggable={false} className="max-w-48 max-h-48 md:max-w-60 md:max-h-60">
-              <Image src={image} width={500} height={500} alt={`Image of ${title} logo`} draggable={false}></Image>
-            </Link>
             <Badge color={color}>
-              <h6>{title}</h6>
-            </Badge>
+                <Link href={link} draggable={false} className="max-w-48 max-h-48 md:max-w-60 md:max-h-60">
+                  <h6>{title}</h6>
+                </Link>
+              </Badge>
           </div>
         ))}
       </div>
